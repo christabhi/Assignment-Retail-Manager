7748ad82f53ab8a53efaf725baf68a3c07b9b4c6
Abhishek Verma <regav25@gmail.com>
2016-11-13 14:43:41 +0530

    Commit first basic controller

---
+++ retail-manager/src/main/java/com/group/retail/rest/controller/ManageShopController.java
@@ -0,0 +1,54 @@
+package com.group.retail.rest.controller;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The DirectoryController class is a RESTful web service controller. The
+ * <code>@RestController</code> annotation informs Spring that each
+ * <code>@RequestMapping</code> method returns a <code>@ResponseBody</code>
+ * which, by default, contains a ResponseEntity converted into JSON with an
+ * associated HTTP status code.
+ * 
+ * @author Abhishek Verma
+ */
+@RestController
+@RequestMapping(value = "/admin/api/", produces = MediaType.APPLICATION_JSON_VALUE)
+public class ManageShopController {
+
+	/**
+	 * The logger for this class hierarchy
+	 */
+	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	/**
+     * Web service endpoint to create new Shop resource. The service returns
+     * the status of the newly created resource as JSON.
+     * @URI: /admin/api/v1/create
+     * @RequestBody: 
+     * {  
+   	 *		"shopName":"testShop",
+   	 *		"shopAddress":{  
+     *			"number":123456,
+     * 			"postCode":411014
+   	 *		}
+	 *	}
+     * @return A ResponseEntity contains the status of resource.
+     */
+	@RequestMapping(value = "v1/create", method = RequestMethod.POST)
+	public ResponseEntity<String> createShopDetails(@Valid @RequestBody String body) {
+		
+		logger.info("> Create Shop Details");
+		
+		return new ResponseEntity<String>("Resources Created Successfully.", HttpStatus.CREATED);
+	}
+}
