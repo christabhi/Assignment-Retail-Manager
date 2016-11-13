54729977e0b4c68abaf6f310e5a8483d9adc0312
Abhishek Verma <regav25@gmail.com>
2016-11-13 14:12:40 +0530

    Integrate Springboot with initial setup

---
+++ retail-manager/src/main/java/com/group/retail/Bootstrap.java
@@ -0,0 +1,58 @@
+package com.group.retail;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.guava.GuavaCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Spring Boot main application class. Serves as both the runtime application
+ * entry point and the central Java configuration class.
+ *
+ * @author Abhishek Verma
+ */
+
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@EnableTransactionManagement
+@EnableCaching
+@ComponentScan("com.group.retail")
+public class Bootstrap {
+
+private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+	
+	/**
+     * Entry point for the application.
+     * 
+     * @param args Command line arguments.
+     * @throws Exception Thrown when an unexpected Exception is thrown from the
+     *         application.
+     */
+	public static void main(String[] args) {
+		
+		logger.info(String.format("Start the Application Container", "Start"));
+		SpringApplication.run(Bootstrap.class, args);
+	}
+	
+	/**
+     * Create a CacheManager implementation class to be used by Spring where
+     * <code>@Cacheable</code> annotations are applied.
+     * 
+     * @return A CacheManager instance.
+     */
+	@Bean
+    public CacheManager cacheManager() 
+	{
+        GuavaCacheManager cacheManager = new GuavaCacheManager("application");
+        return cacheManager;
+    }
+}
